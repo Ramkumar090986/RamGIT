e18c2fcff28bcb84c38d480c6288e0124a563e8f
Ramkumar090986 <ramkumar8687@gmail.com>
2021-04-15 10:43:49 +0530

    Branch2 code

--- src/org/sample/SampleGIT.java
+++ src/org/sample/SampleGIT.java
@@ -3,6 +3,10 @@ package org.sample;
 public class SampleGIT {
 	public static void main(String[] args) {
 		System.out.println("Welcome to GIT");
+		System.out.println("Branch2 code");
+		int a=10,b=10,sum;
+		sum=a+b;
+		System.out.println("Sum of two variables:"+sum);
 	}
 
 }
